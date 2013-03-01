e92a9e9d66028e90ea24eab5a503918bfb1cd2f2
Simon Spicer <mlungu@uw.edu>
2013-03-01 12:24:49 -1000

    Added documentation headers in segment.py

--- sage/rings/polynomial/padics/factor/segment.py
+++ sage/rings/polynomial/padics/factor/segment.py
@@ -4,6 +4,12 @@ from sage.rings.arith import gcd
 class Segment:
 
     def __init__(self,frame,slope,vert,length):
+        """
+        Initialises self.
+
+        EXAMPLES::
+
+        """
         self.frame = frame
         self.vertex = vert
         self.slope = slope
@@ -12,26 +18,59 @@ class Segment:
         if self.frame.E * self.frame.F * self.Eplus < self.frame.Phi.degree():
             self.psi = self.frame.find_psi(self.slope*self.Eplus)
             self._associate_polynomial = self.associate_polynomial(cached=False)
-            self.factors = [AssociatedFactor(self,afact[0],afact[1]) for afact in list(self._associate_polynomial.factor())]
+            self.factors = [AssociatedFactor(self,afact[0],afact[1]) 
+                            for afact in list(self._associate_polynomial.factor())]
         else:
             # Polynomial is irreducible
             return
 
     def h(self):
+        """
+        Return the numerator of the slope of this segment.
+
+        EXAMPLES::
+
+        """
         if isinstance(self.slope,int):
             return self.slope.numerator
         else:
             return self.slope.numerator()
     def e(self):
+        """
+        Return the denominator of the slope of this segment.
+
+        EXAMPLES::
+
+        """
         if isinstance(self.slope,int):
             return self.slope.denominator
         else:
             return self.slope.denominator()
 
     def vphi(self):
+        """
+        Return the slope of this segment.
+
+        EXAMPLES::
+
+        """
         return self.slope
 
     def associate_polynomial(self,cached=True):
+        """
+        Return the associated polynomial of this segment.
+
+        INPUT:
+
+        - ``cached`` - Boolean; default TRUE. If TRUE, returns the cached associated
+          polynomial. If FALSE, complutes the associated polynomial anew.
+
+        OUTPUT:
+
+        - 
+
+        EXAMPLES:
+        """
         if cached:
             return self._associate_polynomial()
         else:
@@ -48,4 +87,10 @@ class Segment:
             return Az
 
     def __repr__(self):
+        """
+        Representation of self.
+        
+        EXAMPLES::
+
+        """
         return 'Segment of length '+repr(self.length)+' and slope '+repr(self.slope)
